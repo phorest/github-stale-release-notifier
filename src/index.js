341575915619b72a21330bd4d63145f362c6a4d9
Dariusz Lesniak <dariusz.lesniak@phorest.com>
2022-02-15 21:18:14 +0000

    Fix action import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const core = require("@actions/core");
-const verifyLastRelease = require("./stale-release-verifier").default;
+const { verifyLastRelease } = require("./stale-release-verifier");
 
 verifyLastRelease().catch((err) => core.setFailed(err));
